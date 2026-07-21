2e54f3edae0584751fe398fdff1cc7bc7e7715e9
thneri95 <tiago.neri1995@outlook.com>
2026-07-21 12:56:31 -0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-let name = "Tiago";
+let nome = "Aragorn";
+let xp = 15000;
+let nivel = "";
 
-console.log('Hello ' + name + '! Welcome to the project.');
+if (xp < 1000) {
+    nivel = "Ferro";
+
+}
+
+else if (xp <= 2000) {
+    nivel = "Bronze";
+}
+
+else if (xp <= 5000) {
+    nivel = "Prata";
+}
+else if (xp <= 8000) {
+    nivel = "Ouro";
+}
+
+else if (xp <= 9000) {
+    nivel = "Platina";
+}
+
+else if (xp <= 10000) {
+    nivel = "Ascendente";
+}
+
+else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
